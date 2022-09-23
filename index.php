3304bad51ec77c2df8e2286ae4123a986abe1dcd
chswell <nikitastreltsov324@gmail.com>
2022-09-23 17:31:43 +0700

    Начало работы

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Task 1</title>
+</head>
+
+<body>
+
+    <form action="search.php" method="POST">
+        <input type="text" placeholder="Имя">
+        <input type="text" placeholder="Фамилия">
+        <input type="text" placeholder="Отчество">
+        <button type="submit">Далее</button>
+    </form>
+
+</body>
+
+</html>
